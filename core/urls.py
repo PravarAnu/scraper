431bc61f2c898c2456ac0e8d93b6ba1961bc1911
Rupal <rupalgupta951@gmail.com>
2021-05-31 01:24:06 +0530

    Added hackernews

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
+from os import name
 from django.urls import path
 from . import views
 
 app_name = "core"
 urlpatterns=[
     path('', views.index, name='index'),
-    path('yourstory', views.yourstory, name="yourstory")
+    path('yourstory', views.yourstory, name="yourstory"),
+    path('hackernews',views.hackernews,name='hackernews'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,19 +1,30 @@
 from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
+from urllib.request import urlopen
 import datetime
+
 def yourstory(request):
     l = []
     url = 'https://yourstory.com/feed'
     content = requests.get(url).content
     soup = BeautifulSoup(content, "html.parser")
     for n, t in enumerate(soup.findAll("title")):
-        # print(str(n) + " - "+ t.text)
         l.append(str(n) + " - " + t.text)
     l.pop(0)
     todays_date = datetime.datetime.now()
-    return render(request,'core/home.html',{'links':l,'date':todays_date})
+    return render(request,'core/yourstory.html',{'links':l,'date':todays_date})
 
+def hackernews(request):
+    url='https://news.ycombinator.com/'
+    l=[]
+    content = urlopen(url).read()
+    soup = BeautifulSoup(content,'html.parser')
+    for i,tag in enumerate(soup.find_all('td',attrs={'class':'title','valign':''})):
+        l.append(str(i+1)+' - '+tag.text + '\n') if tag.text!='More' else ''
+    todays_date = datetime.datetime.now()
+    return render(request,'core/hackernews.html',{'links':l,'date':todays_date})
+       
 
 def index(request):
     return render(request, 'core/landing.html')
