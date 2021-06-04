75f256aeb6d4059808d0935c1252d590dc6f7e2e
Pravar Anu <pravaranu6@gmail.com>
2021-06-05 00:39:04 +0530

    added latest feedback slot in about page

--- core/models.py
+++ core/models.py
@@ -6,3 +6,7 @@ from django.forms import widgets
 class UserFeedback(models.Model):
     email=models.EmailField(max_length=40)
     feedback=models.CharField(max_length=100)
+    date = models.DateTimeField("Date")
+
+        def __str__(self):
+        return self.feedback
--- core/views.py
+++ core/views.py
@@ -3,6 +3,9 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import datetime
+
+from django.utils import timezone
+
 from .forms import Feedback
 from .models import UserFeedback
 from django.http import HttpResponse
@@ -33,16 +36,21 @@ def index(request):
     return render(request, 'core/landing.html')
 
 def about(request):
+
     if request.method=="POST":
         feedback_obj=Feedback(request.POST)
         if feedback_obj.is_valid():
             email_val=feedback_obj.cleaned_data['email']
             feedback_val=feedback_obj.cleaned_data['feedback']
-            UserFeedback_obj=UserFeedback(email=email_val,feedback=feedback_val)
+            UserFeedback_obj=UserFeedback(email=email_val,feedback=feedback_val,date=timezone.now())
             UserFeedback_obj.save()
             feedback_obj=Feedback()
             
     else:
         feedback_obj=Feedback()
-    return render(request,'core/about.html',{'form':feedback_obj})
+    latest_feedback_list = UserFeedback.objects.order_by("-date")[:5]
+    return render(request,'core/about.html',{
+        'form':feedback_obj,
+        'latest_feedback':latest_feedback_list
+    })
 
